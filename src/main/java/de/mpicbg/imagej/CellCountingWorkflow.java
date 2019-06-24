95d9b560715a5e7e964c2e60ed00591f1c95e8a2
haesleinhuepf <rhaase@mpi-cbg.de>
2019-06-24 13:19:27 +0200

    what we did at the dais Learnathon 2019 in Dresden

--- src/main/java/de/mpicbg/imagej/CellCountingWorkflow.java
+++ src/main/java/de/mpicbg/imagej/CellCountingWorkflow.java
@@ -8,15 +8,31 @@
 
 package de.mpicbg.imagej;
 
+import ij.process.AutoThresholder;
+import net.haesleinhuepf.clij.CLIJ;
+import net.haesleinhuepf.clij.clearcl.ClearCLBuffer;
 import net.imagej.Dataset;
 import net.imagej.ImageJ;
 import net.imagej.ops.OpService;
+import net.imagej.ops.Ops;
+import net.imglib2.Cursor;
+import net.imglib2.IterableInterval;
 import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.algorithm.labeling.ConnectedComponents;
 import net.imglib2.img.Img;
+import net.imglib2.roi.Regions;
+import net.imglib2.roi.labeling.ImgLabeling;
+import net.imglib2.roi.labeling.LabelRegion;
+import net.imglib2.roi.labeling.LabelRegions;
+import net.imglib2.type.logic.BitType;
+import net.imglib2.type.numeric.IntegerType;
 import net.imglib2.type.numeric.RealType;
 import org.scijava.command.Command;
 import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
+import org.scijava.table.DefaultGenericTable;
+import org.scijava.table.DoubleColumn;
+import org.scijava.table.Table;
 import org.scijava.ui.UIService;
 
 import java.io.File;
@@ -32,11 +48,91 @@ public class CellCountingWorkflow<T extends RealType<T>> implements Command {
     @Parameter
     private Dataset currentData;
 
+    @Parameter
+    ImageJ ij;
+
     @Override
     public void run() {
         final Img<T> image = (Img<T>)currentData.getImgPlus();
 
+        CLIJ clij = CLIJ.getInstance();
+
+        ClearCLBuffer gpuInput = clij.push(image);
+        ClearCLBuffer gpuBlurred = clij.create(gpuInput);
+        ClearCLBuffer gpuThresholded = clij.create(gpuBlurred);
+
+        float sigma = 3;
+        clij.op().blur(gpuInput, gpuBlurred, sigma, sigma);
+
+        clij.show(gpuBlurred, "blurred");
+
+        clij.op().automaticThreshold(gpuBlurred, gpuThresholded, AutoThresholder.Method.Otsu.name());
+
+        clij.show(gpuThresholded, "thresholded");
+
+        gpuInput.close();
+        gpuBlurred.close();
+        gpuThresholded.close();
+
+        if (true) return;
+
+        RandomAccessibleInterval blurred = ij.op().filter().gauss(image, 3, 3);
+
+        ij.ui().show(blurred);
+
+        IterableInterval ii = (IterableInterval)blurred;
+
+        IterableInterval thresholded = ij.op().threshold().otsu(ii);
+
+        ij.ui().show(thresholded);
+
+        invertImage(thresholded);
+
+        ij.ui().show(thresholded);
+
+        RandomAccessibleInterval thresholdedRai = ij.convert().convert(thresholded, RandomAccessibleInterval.class);
+
+        ImgLabeling labeling = ij.op().labeling().cca(thresholdedRai, ConnectedComponents.StructuringElement.FOUR_CONNECTED);
+
+        RandomAccessibleInterval indexImage = labeling.getIndexImg();
+        ij.ui().show(indexImage);
+
+        LabelRegions<IntegerType> regions = new LabelRegions(labeling);
+
+        DoubleColumn areaColumn = new DoubleColumn();
+        DoubleColumn intensityColumn = new DoubleColumn();
+
+        for(LabelRegion region : regions) {
+
+            IterableInterval sample = Regions.sample(region, image);
+
+            RealType meanIntensity = ij.op().stats().mean(sample);
+
+            double area = region.size();
+            double mean = meanIntensity.getRealDouble();
+
+            areaColumn.add(area);
+            intensityColumn.add(mean);
+
+        }
+
+        Table table = new DefaultGenericTable();
+        table.add(areaColumn);
+        table.add(intensityColumn);
+
+        ij.ui().show(table);
+
         System.out.println("Hello world!");
+    }
+
+    private void invertImage(IterableInterval thresholded) {
+        Cursor cursor = thresholded.cursor();
+
+        while (cursor.hasNext()) {
+            BitType pixel = (BitType)cursor.next();
+            pixel.set(!pixel.get());
+        }
+
     }
 
     /**
@@ -53,7 +149,8 @@ public class CellCountingWorkflow<T extends RealType<T>> implements Command {
         ij.ui().showUI();
 
         // ask the user for a file to open
-        final File file = ij.ui().chooseFile(null, "open");
+        final File file = new File("C:/structure/data/blobs.tif");
+                // ij.ui().chooseFile(null, "open");
 
         if (file != null) {
             // load the dataset
